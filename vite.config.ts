f82094c33fc6dd5733124046b9e912081a9b503e
chochodidije-hub <chochodidije@gmail.com>
2026-04-01 21:22:12 -0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    // هذا السطر يمنع التعليق اللانهائي
+    minify: false, 
     rollupOptions: {
       input: {
         main: './index.html',
